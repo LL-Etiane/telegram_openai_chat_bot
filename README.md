d9f231a3cba5a53414ca71fef93375e0ffb59dc8
LL-Etiane <lukongleinyuyetiane@gmail.com>
2023-03-15 16:38:50 +0100

    initial project

---
+++ README.md
@@ -0,0 +1,5 @@
+# A simple open ai chat bot in telgram
+#### How to run
+- install the node modules with `npm install`
+- create a  `.env` file with your open ai api key and telegram bot token
+- `node index.js`
---
+++ index.js
@@ -0,0 +1,31 @@
+require("dotenv").config()
+const { Configuration, OpenAIApi } = require("openai")
+const { Telegraf } = require("telegraf")
+const { message } = require("telegraf/filters")
+
+const bot = new Telegraf(process.env.TELEGRAM_BOT_TOKEN)
+
+const configuration = new Configuration({ apiKey: process.env.OPENAI_API_KEY })
+const openai = new OpenAIApi(configuration)
+
+bot.start((ctx) =>{
+    ctx.reply("Welcome.\n Send me a message to get started")
+})
+
+bot.on(message, async(ctx) =>{
+    try {
+        const completion = await openai.createCompletion({
+            model: "text-davinci-003",
+            prompt: ctx.message.text
+        })
+        ctx.reply(completion.data.choices[0].text)
+    } catch (error) {
+        ctx.reply("Something went wrong. Please try again later")
+        console.log(error)
+    }
+})
+
+bot.launch()
+// Enable graceful stop
+process.once('SIGINT', () => bot.stop('SIGINT'));
+process.once('SIGTERM', () => bot.stop('SIGTERM'));
